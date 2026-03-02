b4bb5fc42c74d99535b386f44ba513ed6918b9a6
Shekwara <socratesshekwara46@gmail.com>
2026-03-02 15:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import re
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def predict():
     return render_template("index.html", prediction_text=result)
 
 if __name__ == "__main__":
+
    app.run(host="0.0.0.0", port=10000)
